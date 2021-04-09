815531afbc9d266370d3a9064781a7bcfb6fe4fc
saugatksh <69414867+saugatksh@users.noreply.github.com>
2021-04-09 08:16:44 +0545

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+ 	
+			<form action="registration.php" method="post">
+			<h3>Create an account</h3>
+				<label>Name</label>
+				<input type="text" name="pname"  required><br><br>
+			
+			
+				<label>Phone</label>
+				<input type="number" name="pnum"  required><br><br>
+			
+			<button type="submit" class="btn btn-primary">Sign up</button>	
+
+			</form>
+	
+</body>
+</html>
